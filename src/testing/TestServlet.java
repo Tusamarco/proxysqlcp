4ed419a5f76ab1bfdf9aab0bd46cfe17a1280a26
Marco Tusa <marco.tusa@shopify.com>
2016-12-05 14:49:06 -0500

    test

--- src/testing/TestServlet.java
+++ src/testing/TestServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class TestServlet
+ * Servlet implementation class TestServlet test
  */
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
