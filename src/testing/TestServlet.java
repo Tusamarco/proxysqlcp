0175d2b2df9cd5e40434d483f68dc15c3f9d8fed
Marco Tusa <marco.tusa@shopify.com>
2016-12-05 15:23:14 -0500

    clean crap

--- src/testing/TestServlet.java
+++ src/testing/TestServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class TestServlet test4
+ * Servlet implementation class TestServlet 
  */
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
